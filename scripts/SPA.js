45f110a02c3e500a8190f26f101eaf967775da35
Frillion <fridriksoebech@gmail.com>
2022-11-28 12:40:02 +0000

    testing Stuff had commenced

---
+++ scripts/SPA.js
@@ -0,0 +1,6 @@
+"use strict";
+document.addEventListener("DOMContentLoaded",domloaded);
+function domloaded(){
+    const price_filter = document.getElementById("priceFilter");
+    price_filter.setAttribute("max","50")
+}
