cb3d6933aaf837bbde445e8630d84fdbbf140da2
Frillion <fridriksoebech@gmail.com>
2022-11-28 15:02:23 +0000

    Testing out async fetch

--- scripts/SPA.js
+++ scripts/SPA.js
@@ -2,5 +2,13 @@
 document.addEventListener("DOMContentLoaded",domloaded);
 function domloaded(){
     const price_filter = document.getElementById("priceFilter");
-    price_filter.setAttribute("max","50")
+    price_filter.setAttribute("max","50");
+    async function FetchItems(){
+        const url = "https://gist.githubusercontent.com/GunnarThorunnarson/4c7e296af3437290e00209551d79a760/raw/4d90d3dd652bd4e3ea046cfb0ffc2bb1745c5771/data.json";
+        try{
+            const response = await fetch(url);
+            return response.json();
+        }catch(err){console.log(err);}
+    }
+   console.log(items); 
 }
