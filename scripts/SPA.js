5e0ba6aa1c05eed1ceffcfaf25a29482c6264382
Frillion <fridriksoebech@gmail.com>
2022-12-01 22:36:44 +0000

    The Calender Search Is Finally Done And Therefore The Entire Course

--- scripts/SPA.js
+++ scripts/SPA.js
@@ -17,6 +17,30 @@ async function domloaded(){
             }
         });
     }
+    function findMinDate(ls){
+        let min_date = new Date(ls[0].date);
+        let return_min_date = undefined;
+        for(let i=0; i<ls.length; i++){
+            const comparison_date= new Date(ls[i].date);
+            if(min_date.getTime() > comparison_date.getTime()){
+                min_date = new Date(ls[i].date);
+                return_min_date = ls[i].date;
+            }
+        }
+        return return_min_date;
+    }
+    function findMaxDate(ls){
+        let max_date = new Date(ls[0].date);
+        let return_max_date = undefined;
+        for(let i=0; i<ls.length; i++){
+            const comparison_date = new Date(ls[i].date);
+            if(max_date.getTime() < comparison_date.getTime()){
+                max_date = new Date(ls[i].date);
+                return_max_date = ls[i].date;
+            }
+        }
+        return return_max_date;
+    }
     function filterArtist(){
         for(let i=0; i<artist.length; i++){
             if(!artist[i].innerText.toLowerCase().includes(artist_filter.value.toLowerCase())&&!filtered_items.includes(artist[i])&&artist_filter.value != ""){
@@ -28,6 +52,24 @@ async function domloaded(){
         for(let i=0; i<art.length;i++){
             if(!art[i].innerText.toLowerCase().includes(art_filter.value.toLowerCase())&&!filtered_items.includes(art[i])&&art_filter.value != ""){
                 filtered_items.push(art[i]);
+            }
+        }
+    }
+    function filterStartDate(){
+        const start_date = new Date(start_date_filter.value);
+        for(let i=0; i<production_dates.length; i++){
+            const compare_date = new Date(production_dates[i].innerText);
+            if(compare_date.getTime() < start_date.getTime()&&!filtered_items.includes(production_dates[i])&&start_date!=""){
+                filtered_items.push(production_dates[i]);
+            }
+        }
+    }
+    function filterEndDate(){
+        const end_date = new Date(end_date_filter.value);
+        for(let i=0; i<production_dates.length; i++){
+            const compare_date = new Date(production_dates[i].innerText);
+            if(compare_date.getTime() > end_date.getTime()&&!filtered_items.includes[production_dates[i]]&&end_date!=""){
+                filtered_items.push(production_dates[i]);
             }
         }
     }
@@ -79,7 +121,7 @@ async function domloaded(){
      Based On All Filter Parameters using the filtered_items List*/
     function setHidden(){
         for(let i=0; i<pricers.length;i++){
-            if(filtered_items.includes(pricers[i])||filtered_items.includes(artist[i])||filtered_items.includes(art[i])){
+            if(filtered_items.includes(pricers[i])||filtered_items.includes(artist[i])||filtered_items.includes(art[i])||filtered_items.includes(production_dates[i])){
                 pricers[i].parentElement.parentElement.setAttribute("class",'hideItem');
             }else{
                 pricers[i].parentElement.parentElement.setAttribute("class","w3-col l3 m6 w3-margin-bottom");
@@ -88,10 +130,27 @@ async function domloaded(){
     }
     /*Empties The filtered_items List So That setHidden() Can Show The Item Again*/
     function emptyFilter(){
+        const start_compare_date = new Date(start_date_filter.value);
+        const end_compare_date = new Date(end_date_filter.value);
         for(let i = 0; i < filtered_items.length;i++){
-            if(filtered_items[i].innerText >= price_display.innerText&&filtered_items[i].innerText.toLowerCase().includes(artist_filter.value.toLowerCase())&&filtered_items[i].innerText.toLowerCase().includes(art_filter.value.toLowerCase())){
+            const date = new Date(filtered_items[i].innerText);
+            if(filtered_items[i].innerText >= price_display.innerText&&filtered_items[i].innerText.toLowerCase().includes(artist_filter.value.toLowerCase())&&filtered_items[i].innerText.toLowerCase().includes(art_filter.value.toLowerCase())&&date instanceof Date && isNaN(date.valueOf())){
                 filtered_items.splice(i,1);
             }
+            else if(date instanceof Date && !isNaN(date.valueOf())&&(end_compare_date.getTime() >= date.getTime() && date.getTime() >= start_compare_date.getTime())&&(end_date_filter.value!=""&&start_date_filter.value!="")){
+                filtered_items.splice(i,1);
+            }
+            else if(date instanceof Date && !isNaN(date.valueOf())&&(end_compare_date.getTime() >= date.getTime() || date.getTime() >= start_compare_date.getTime())&&(end_date_filter.value==""||start_date_filter.value=="")){
+                filtered_items.splice(i,1);
+            }
+        }
+    }
+    function dynamicCalender(){
+        if(start_date_filter.value!=""){
+            end_date_filter.setAttribute("min",start_date_filter.value);
+        }
+        if(end_date_filter.value!=""){
+            start_date_filter.setAttribute("max",end_date_filter.value);
         }
     }
     /*Custom Comparative Function So That Items Can Be Sorted With array.prototype.sort()*/
@@ -110,8 +169,16 @@ async function domloaded(){
     const price_display = document.getElementById("priceDisplay");
     const artist_filter = document.getElementById("artistFilter");
     const art_filter = document.getElementById("artFilter");
+    const start_date_filter = document.getElementById("startDateFilter");
+    const end_date_filter = document.getElementById("endDateFilter");
+    start_date_filter.setAttribute("min",findMinDate(items));
+    start_date_filter.setAttribute("max",findMaxDate(items));
+    end_date_filter.setAttribute("min",findMinDate(items));
+    end_date_filter.setAttribute("max",findMaxDate(items));
     artist_filter.addEventListener("input",filterArtist);
     art_filter.addEventListener("input",filterArt);
+    start_date_filter.onchange = filterStartDate;
+    end_date_filter.addEventListener("input",filterEndDate);
     initSlider(0,items.length-1);
     const item_container = document.createElement("div");
     const item_list = document.createElement("div");
@@ -126,4 +193,5 @@ async function domloaded(){
     document.getElementById("pageContent").appendChild(item_container);
     window.setInterval(setHidden,100);
     window.setInterval(emptyFilter,100);
+    window.setInterval(dynamicCalender,100);
 }
